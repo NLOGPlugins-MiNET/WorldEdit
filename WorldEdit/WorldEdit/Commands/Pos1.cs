6b4c11806c58518f65fce122f62fc1591682221f
Herb9 <pksxtm@gmail.com>
2017-08-12 21:26:18 +0900

    Pos1 command included.

---
+++ WorldEdit/WorldEdit/Commands/Pos1.cs
@@ -0,0 +1,40 @@
+﻿
+/*
+    ▓█████▄  ▄▄▄       ██▀███   ██ ▄█▀ ██▓     █    ██  ▄▄▄▄   
+    ▒██▀ ██▌▒████▄    ▓██ ▒ ██▒ ██▄█▒ ▓██▒     ██  ▓██▒▓█████▄ 
+    ░██   █▌▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░ ▒██░    ▓██  ▒██░▒██▒ ▄██
+    ░▓█▄   ▌░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄ ▒██░    ▓▓█  ░██░▒██░█▀  
+    ░▒████▓  ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄░██████▒▒▒█████▓ ░▓█  ▀█▓
+     ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒░ ▒░▓  ░░▒▓▒ ▒ ▒ ░▒▓███▀▒
+     ░ ▒  ▒   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░░ ░ ▒  ░░░▒░ ░ ░ ▒░▒   ░ 
+     ░ ░  ░   ░   ▒     ░░   ░ ░ ░░ ░   ░ ░    ░░░ ░ ░  ░    ░ 
+       ░          ░  ░   ░     ░  ░       ░  ░   ░      ░      
+     ░                                                       ░ 
+
+     WorldEdit by Herb9. (pksxtm@gmail.com, Telegram @Herb9)
+*/
+
+using MiNET.Utils;
+using MiNET.Plugins.Attributes;
+using MiNET;
+
+namespace WorldEdit.Commands
+{
+    public class Pos1
+    {
+        private WorldEdit Plugin { get; set; }
+
+        public Pos1(WorldEdit plugin)
+        {
+            Plugin = plugin;
+        }
+
+        [Command(Name = "pos1", Description = "Sets your first position.", Permission = "worldedit.commands.pos1")]
+        public void SetFirstPosition(Player sender)
+        {
+            Plugin.SetFirstPosition(sender);
+
+            sender.SendMessage(ChatColors.Gray + "First position set to X: " + (int) sender.KnownPosition.X + " Y: " + (int) sender.KnownPosition.Y + " Z: " + (int) sender.KnownPosition.Z);
+        }
+    }
+}
--- WorldEdit/WorldEdit/WorldEdit.cs
+++ WorldEdit/WorldEdit/WorldEdit.cs
@@ -14,10 +14,15 @@
      WorldEdit by Herb9. (pksxtm@gmail.com, Telegram @Herb9)
 */
 
+using WorldEdit.Commands;
+
+using MiNET.Utils;
 using MiNET.Plugins.Attributes;
 using MiNET.Plugins;
 using MiNET;
 
+using System.Collections.Generic;
+using System.Text;
 using System;
 
 namespace WorldEdit
@@ -25,9 +30,44 @@ namespace WorldEdit
     [Plugin(PluginName = "WorldEdit", Description = "Simple and fastest world editing tools for MiNET.", PluginVersion = "1.0", Author = "Herb9")]
     public class WorldEdit : Plugin
     {
+        public const string Prefix = "\x5b\x57\x6f\x72\x6c\x64\x45\x64\x69\x74\x5d";
+
+        public Dictionary<Player, BlockCoordinates> Pos1 = new Dictionary<Player, BlockCoordinates>();
+
         protected override void OnEnable()
         {
-            Console.WriteLine("WorldEdit v1.0 by Herb9. (pksxtm@gmail.com, Telegram @Herb9)");
+            RegisterCommands();
+
+            Console.WriteLine(Prefix + Encoding.UTF8.GetString(Convert.FromBase64String("IFdvcmxkRWRpdCB2MS4wIGJ5IEhlcmI5LiAocGtzeHRtQGdtYWlsLmNvbSwgVGVsZWdyYW0gQEhlcmI5KQ==")));
+        }
+
+        private void RegisterCommands()
+        {
+            Context.PluginManager.LoadCommands(new Pos1(this));
+        }
+
+        /*
+             ▄▄▄       ██▓███   ██▓
+            ▒████▄    ▓██░  ██▒▓██▒
+            ▒██  ▀█▄  ▓██░ ██▓▒▒██▒
+            ░██▄▄▄▄██ ▒██▄█▓▒ ▒░██░
+             ▓█   ▓██▒▒██▒ ░  ░░██░
+             ▒▒   ▓▒█░▒▓▒░ ░  ░░▓  
+              ▒   ▒▒ ░░▒ ░      ▒ ░
+              ░   ▒   ░░        ▒ ░
+                  ░  ░          ░  
+        */
+
+        public bool IsFirstPosition(Player player) => Pos1.ContainsKey(player);
+
+        public void SetFirstPosition(Player player)
+        {
+            Pos1[player] = new BlockCoordinates(player.KnownPosition);
+        }
+
+        public void RemoveFirstPosition(Player player)
+        {
+            Pos1.Remove(player);
         }
     }
 }
