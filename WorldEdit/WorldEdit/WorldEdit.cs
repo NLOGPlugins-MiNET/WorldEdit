bcbbffe3ee22da1d3fd1904e1328db0b597fc7a5
Herb9 <pksxtm@gmail.com>
2017-08-12 18:52:14 +0900

    Initial commit

---
+++ WorldEdit/WorldEdit/WorldEdit.cs
@@ -0,0 +1,33 @@
+﻿
+/*
+    ▓█████▄  ▄▄▄       ██▀███   ██ ▄█▀ ██▓     █    ██  ▄▄▄▄   
+    ▒██▀ ██▌▒████▄    ▓██ ▒ ██▒ ██▄█▒ ▓██▒     ██  ▓██▒▓█████▄ 
+    ░██   █▌▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░ ▒██░    ▓██  ▒██░▒██▒ ▄██
+    ░▓█▄   ▌░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄ ▒██░    ▓▓█  ░██░▒██░█▀  
+    ░▒████▓  ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄░██████▒▒▒█████▓ ░▓█  ▀█▓
+     ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒░ ▒░▓  ░░▒▓▒ ▒ ▒ ░▒▓███▀▒
+     ░ ▒  ▒   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░░ ░ ▒  ░░░▒░ ░ ░ ▒░▒   ░ 
+     ░ ░  ░   ░   ▒     ░░   ░ ░ ░░ ░   ░ ░    ░░░ ░ ░  ░    ░ 
+       ░          ░  ░   ░     ░  ░       ░  ░   ░      ░      
+     ░                                                       ░ 
+
+     WorldEdit by Herb9. (pksxtm@gmail.com, Telegram @Herb9)
+*/
+
+using MiNET.Plugins.Attributes;
+using MiNET.Plugins;
+using MiNET;
+
+using System;
+
+namespace WorldEdit
+{
+    [Plugin(PluginName = "WorldEdit", Description = "Simple and fastest world editing tools for MiNET.", PluginVersion = "1.0", Author = "Herb9")]
+    public class WorldEdit : Plugin
+    {
+        protected override void OnEnable()
+        {
+            Console.WriteLine("WorldEdit v1.0 by Herb9. (pksxtm@gmail.com, Telegram @Herb9)");
+        }
+    }
+}
